fd64b765241e9a210f9e10ec2f52c3e37030894a
Marek Hulan <mhulan@redhat.com>
2017-11-29 10:53:42 +0100

    Refs #18292 - make it more resilient

--- app/services/ui_notifications/rss_notifications_checker.rb
+++ app/services/ui_notifications/rss_notifications_checker.rb
@@ -3,6 +3,28 @@ require 'date'
 
 module UINotifications
   class RssNotificationsChecker
+    class Item
+      def initialize(feed_item)
+        @item = feed_item
+      end
+
+      def link
+        @link ||= @item.link.respond_to?(:href) ? @item.link.href : @item.link
+      end
+
+      def summary
+        @summary ||= begin
+          summary = @item.summary if @item.respond_to?(:summary)
+          summary = @item.description if @item.respond_to?(:description)
+          summary.respond_to?(:content) ? summary.content : summary
+        end
+      end
+
+      def title
+        @title ||= @item.title.respond_to?(:content) ? @item.title.content : @item.title
+      end
+    end
+
     def initialize(options = [])
       @url = options[:url] || Setting[:rss_url]
       @latest_posts = options[:latest_posts] || 3
@@ -15,7 +37,8 @@ module UINotifications
       # gets enabled, notifications for RSS feeds are created again
       return true unless Setting[:rss_enable]
       feed = RSS::Parser.parse(@url, false)
-      feed.items[0, @latest_posts].each do |item|
+      feed.items[0, @latest_posts].each do |feed_item|
+        item = Item.new(feed_item)
         blueprint = rss_notification_blueprint
         if notification_already_exists?(item)
           next unless @force_repost
@@ -23,12 +46,12 @@ module UINotifications
         Notification.create(
           :initiator => User.anonymous_admin,
           :audience => @audience,
-          :message => item.title.content,
+          :message => item.title,
           :notification_blueprint => blueprint,
           :actions => {
             :links => [
               {
-                :href => item.link.href,
+                :href => item.link,
                 :title => _('Open'),
                 :external => true
               }
@@ -45,7 +68,7 @@ module UINotifications
     end
 
     def notification_already_exists?(item)
-      !!Notification.unscoped.find_by_message(item.summary.content)
+      !!Notification.unscoped.find_by_message(item.summary)
     end
   end
 end
